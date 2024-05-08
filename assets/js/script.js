d3200af12cccf8b2759fa9323f79f5e953da1cd6
Godwin C. Naces <godwinnaces@gmail.com>
2024-05-08 10:01:28 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,11 @@ function checkAnswer() {
 
     if(isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
+
     } else {
         alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -74,12 +77,21 @@ function calculateCorrectAnswer() {
     }
 
 }
-
+/**
+ * Get the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
-}
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
+}
+/**
+ * Get the current tally of incorrect from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
